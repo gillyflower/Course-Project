7839d827cc621f094327738f166d4e38097a5c03
Gillian <gillyflower2@me.com>
2016-05-20 12:14:39 +0200

    Change "output" to "print_cd" and incorporate use of cd_t

--- Course-Project/main.c
+++ Course-Project/main.c
@@ -54,26 +54,25 @@ cd_t read_cd ()                                           /* Function of type cd
 }
 
 
-
-
-
                                         /* ------ Print to Screen -------*/
 
-int output (char title[], char artist[], int tracks, int album, float price)
+int print_cd (cd_t cd)
 {
     puts ("=======================================");    /* Note puts automatically adds a newline to the output */
-    printf ("Title: %s\n", title);
+    printf ("Title: %s\n", cd.title);
 #ifndef NOARTIST                                         /* This is an ifdef defined in the header file to ignore this code if NOARTIST is defined */
-    printf ("Artist: %s\n", artist);
+    printf ("Artist: %s\n", cd.artist);
 #endif
-    printf ("Number of Tracks: %d\n", tracks);
-    puts (album? "Album":"Single");
-    printf ("Price: %.2f\n", price);
+    printf ("Number of Tracks: %d\n", cd.tracks);
+    puts (cd.album? "Album":"Single");
+    printf ("Price: %.2f\n", cd.price);
     puts ("========================================\n");
     
     return 0;                                       /* NOTE: Mark shows no retun value but the compiler requires it
                                                      (control reaches end of non void function) so I am retuning 0 */
 }
+
+
 
                                         /* ------ Prompt User -------*/
 
